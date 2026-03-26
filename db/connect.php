1989c57bbdb6d2cb3c8102c552860702a97ced7e
amrutha1215 <123935910+amrutha1215@users.noreply.github.com>
2026-03-26 09:27:31 +0530

    Update connect.php

--- db/connect.php
+++ db/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "eventmgmt";
+$servername = "sql201.infinityfree.com";
+$username = "if0_41479526";
+$password = "a1m2r3u4t5h6a7";
+$db = "if0_41479526_db_campusconnect";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db);
